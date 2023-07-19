903c8563ed4b0b94a2e25c4e0105156ac0d00292
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2023-07-19 16:09:54 +0530

    collegeAdded

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Card from './UI/Card';
+import Card from '../UI/Card';
 import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
@@ -9,7 +9,7 @@ const UsersList = (props) => {
       <ul>
         {props.users.map((user) => (
           <li key={user.id}>
-            {user.name} ({user.age} years old)
+            {user.name} ({user.college}) - {user.age} years old
           </li>
         ))}
       </ul>
